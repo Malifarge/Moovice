36efae4975d382c25125b5ea058dbd9c926ef09f
Thomas <malifarge.thomas9@gmail.com>
2022-08-03 16:59:21 +0200

    favorite

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,39 @@
 
+
 const Card = (props) =>{
-    console.log(props.movies);
+
+
+    const handleAddClick = (ID) =>{
+
+        let Ids
+    
+        if (localStorage.favoriteIds){
+            const localStorageIds= localStorage.getItem("favoriteIds")
+       Ids = JSON.parse(localStorageIds)
+        }else{
+           Ids=[]
+        }
+        const newId= Ids.find((id)=>{
+            return id === ID
+        })
+        if(!newId){
+            Ids.push(ID)
+        }
+        const stringifiedIds = JSON.stringify(Ids)
+        localStorage.setItem('favoriteIds', stringifiedIds)
+    }
+
    return <>
    { props.movies.map((movie)=>{
+        // console.log(movie.id);
         return <article key={movie.title}>
         <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
         <h2>{movie.title}</h2>
         <p>release : {movie.release_date}</p>
         <h3>About</h3>
         <p>{movie.overview}</p>
+
+        <button onClick={()=>handleAddClick(movie.id)}>Add to favorite</button>
         </article>
     })}
    </>
--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,11 +1,58 @@
 
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useState, useEffect } from "react"
 import NavBar from "../components/navBar"
 
 const Favorites = () =>{
+
+    const [movies, setMovies] = useState([])
+    const [moviesList,setMoviesList] = useState([])
+
+    const fetchMovies = async () => {
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fe35d13ec177e4465861d822f792c0a9`)
+        const data = await response.json()
+        setMoviesList(data.results)
+      }
+
+    useEffect(()=>{
+    fetchMovies()
+  },[]
+  )
+
+  useEffect(()=>{
+    let Ids
+    const newMovie = movies
+        if (localStorage.favoriteIds){
+            const localStorageIds= localStorage.getItem("favoriteIds")
+       Ids = JSON.parse(localStorageIds)
+        }else{
+           Ids=[]
+        }
+    Ids.forEach((id)=>{
+        const currentMovie = moviesList.find(movieList => movieList.id === Number(id))
+        if(currentMovie){
+            newMovie.push(currentMovie)
+            setMovies(newMovie)
+        }
+    })
+  },[moviesList]
+  )
+
+
     return(
         <>
         <NavBar/>
         <h1>Favorites</h1>
+        {movies.map((movie)=>{
+          return  <article key={movie.title}>
+            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
+        <h2>{movie.title}</h2>
+        <p>release : {movie.release_date}</p>
+        <h3>About</h3>
+        <p>{movie.overview}</p>
+          </article>
+        })}
         </>
     )
 }

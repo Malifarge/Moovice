a1182f9dc06f1a23750fb7a0a5406f3eaa75a71e
Thomas <malifarge.thomas9@gmail.com>
2022-08-04 11:41:34 +0200

    nav tel amelioration

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -6,9 +6,9 @@ const NavBar = () =>{
     return(
         <nav>
             <Link to={`/`}>home</Link>
-            <Link to={`/weekly`}>Weekly</Link>
-            <Link to={`/popular`}>Popular</Link>
-            <Link to={`/favorites`}>Favorites</Link>
+            <Link to={`/weekly`} className='mleft'>Weekly</Link>
+            <Link to={`/popular`} className='mleft'>Popular</Link>
+            <Link to={`/favorites`} className='mleft'>Favorites</Link>
         </nav>
     )
 }

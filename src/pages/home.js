fef811fe38aff396483785374592a6c526573997
Thomas <malifarge.thomas9@gmail.com>
2022-08-05 10:42:43 +0200

    Top Rated

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,17 +5,21 @@ import NavBar from "../components/navBar"
 import '../styles/home.css'
 
 const Home = () =>{
+    const apiKey = "fe35d13ec177e4465861d822f792c0a9"
+    const [moviesNowPlaying,setMoviesNowPlaying]= useState([])
+    const [moviesTopRated,setMoviesTopRated]= useState([])
 
-    const [movies,setMovies]= useState([])
-
-    const fetchMoviesTest = async () => {
-        const response = await fetch(` https://api.themoviedb.org/3/movie/now_playing?api_key=fe35d13ec177e4465861d822f792c0a9&language=en-US&page=1`)
-        const data = await response.json()
-        setMovies(data.results)
+    const fetchMovies = async () => {
+        const responsePlaying = await fetch(` https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`)
+        const dataPlaying = await responsePlaying.json()
+        setMoviesNowPlaying(dataPlaying.results)
+        const responseTop = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+        const dataTop = await responseTop.json()
+        setMoviesTopRated(dataTop.results)
       }
 
     useEffect(()=>{
-        fetchMoviesTest()
+        fetchMovies()
     },[])
 
     const handleAddClick = (ID) =>{
@@ -40,9 +44,9 @@ const Home = () =>{
         <NavBar/>
         <section className="home">
             <h1>Home</h1>
-        <h2>Now playing</h2>
+            <h2>Now playing</h2>
             <section className="playing">
-                {movies.map((movie)=>{
+                {moviesNowPlaying.map((movie)=>{
                     return <article className="weeklyArticle" key={movie.title}>
                         <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
                         <h3>{movie.title}</h3>
@@ -50,6 +54,17 @@ const Home = () =>{
                     </article>
                  })}
             </section>
+            <h2>Top Rated</h2>
+            <section className="playing">
+                {moviesTopRated.map((movie)=>{
+                    return <article className="weeklyArticle" key={movie.title}>
+                        <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
+                        <h3>{movie.title}</h3>
+                        <button onClick={()=>handleAddClick(movie.id)}>Add to favorite</button>
+                    </article>
+                 })}
+            </section>
+
         </section>
         </>
     )

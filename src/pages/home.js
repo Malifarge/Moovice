a856442989288d069746e86bbb0fadfb97026014
Thomas <malifarge.thomas9@gmail.com>
2022-08-05 10:52:42 +0200

    Upcoming

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,8 @@ const Home = () =>{
     const apiKey = "fe35d13ec177e4465861d822f792c0a9"
     const [moviesNowPlaying,setMoviesNowPlaying]= useState([])
     const [moviesTopRated,setMoviesTopRated]= useState([])
+    const [moviesUpComing,setMoviesUpComing]= useState([])
+
 
     const fetchMovies = async () => {
         const responsePlaying = await fetch(` https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`)
@@ -16,6 +18,9 @@ const Home = () =>{
         const responseTop = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
         const dataTop = await responseTop.json()
         setMoviesTopRated(dataTop.results)
+        const responseUpcoming = await fetch (`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`)
+        const dataUpcoming = await responseUpcoming.json()
+        setMoviesUpComing(dataUpcoming.results)
       }
 
     useEffect(()=>{
@@ -44,6 +49,7 @@ const Home = () =>{
         <NavBar/>
         <section className="home">
             <h1>Home</h1>
+
             <h2>Now playing</h2>
             <section className="playing">
                 {moviesNowPlaying.map((movie)=>{
@@ -54,9 +60,21 @@ const Home = () =>{
                     </article>
                  })}
             </section>
+
             <h2>Top Rated</h2>
             <section className="playing">
                 {moviesTopRated.map((movie)=>{
+                    return <article className="weeklyArticle" key={movie.title}>
+                        <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
+                        <h3>{movie.title}</h3>
+                        <button onClick={()=>handleAddClick(movie.id)}>Add to favorite</button>
+                    </article>
+                 })}
+            </section>
+
+            <h2>Upcoming</h2>
+            <section className="playing">
+                {moviesUpComing.map((movie)=>{
                     return <article className="weeklyArticle" key={movie.title}>
                         <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title}/>
                         <h3>{movie.title}</h3>
